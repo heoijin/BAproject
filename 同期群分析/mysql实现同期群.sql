af2256b2b4fece4c21733622028a17cfe6a1551b
heoijin <heoijin@163.com>
2020-04-25 00:25:43 +0800

    更新留存率展示方式

--- 同期群分析/mysql实现同期群.sql
+++ 同期群分析/mysql实现同期群.sql
@@ -58,3 +58,60 @@ FROM cohort
 where `月份差`=0
 ) m 
 on c.`首付月份`=m.`首付月份`;
+
+
+
+-- 求留存率，并改变表格样式
+SELECT a.`首付月份`,
+CASE a.`月份差` when 0 THEN a.`留存率` ELSE 0 END 首月,
+CASE a.`月份差` when 1 THEN a.`留存率` ELSE 0 END "+1月",
+CASE a.`月份差` when 2 THEN a.`留存率` ELSE 0 END "+2月",
+CASE a.`月份差` when 3 THEN a.`留存率` ELSE 0 END "+3月",
+CASE a.`月份差` when 4 THEN a.`留存率` ELSE 0 END "+4月",
+CASE a.`月份差` when 5 THEN a.`留存率` ELSE 0 END "+5月"
+FROM(
+SELECT a.`首付月份`,a.`月份差`,ROUND((a.`留存量`/b.`留存量`)*100,2) 留存率
+FROM cohort a
+LEFT JOIN (
+	SELECT `首付月份`,`留存量`
+	FROM cohort
+	WHERE cohort.`月份差`=0
+) b
+on a.`首付月份`=b.`首付月份`
+) a
+
+
+
+
+SELECT 
+	n.`首付月份`,
+	CONCAT(sum(n.`首月`),"%") 首月,
+	CONCAT(sum(n.`+1月`),"%") "+1月",
+	CONCAT(sum(n.`+2月`),"%") "+2月",
+	CONCAT(sum(n.`+3月`),"%") "+3月",
+	CONCAT(sum(n.`+4月`),"%") "+4月",
+	CONCAT(sum(n.`+5月`),"%") "+5月"
+FROM(
+	# 一级子查询：转置表格，将月份差作为列名
+	SELECT 
+		a.`首付月份`,
+		CASE a.`月份差` when 0 THEN a.`留存率` ELSE 0 END 首月,
+		CASE a.`月份差` when 1 THEN a.`留存率` ELSE 0 END "+1月",
+		CASE a.`月份差` when 2 THEN a.`留存率` ELSE 0 END "+2月",
+		CASE a.`月份差` when 3 THEN a.`留存率` ELSE 0 END "+3月",
+		CASE a.`月份差` when 4 THEN a.`留存率` ELSE 0 END "+4月",
+		CASE a.`月份差` when 5 THEN a.`留存率` ELSE 0 END "+5月"
+	FROM(
+		# 二级子查询：计算留存率
+		SELECT a.`首付月份`,a.`月份差`,ROUND((a.`留存量`/b.`留存量`)*100,2) 留存率
+		FROM cohort a
+		LEFT JOIN (
+			# 三级子查询：查询首月用户量
+			SELECT `首付月份`,`留存量`
+			FROM cohort
+			WHERE cohort.`月份差`=0
+	) b
+	on a.`首付月份`=b.`首付月份`
+	) a
+) n
+GROUP BY n.`首付月份`;
