2b1ad8e8f148cd26065cf611d49a4f269a7d9210
heoijin <heoijin@163.com>
2020-06-12 21:01:18 +0800

    update 天猫真实订单分析

--- 天猫真实订单/tmall.py
+++ 天猫真实订单/tmall.py
@@ -2,6 +2,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from pyecharts import options as opts
 import seaborn as sns
+import warnings
 # 漏斗图
 from pyecharts.charts import Funnel
 # 地图
@@ -13,6 +14,7 @@ from snapshot_selenium import snapshot
 plt.rcParams['font.sans-serif']=['SimHei'] # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False # 用来正常显示负号
 pd.set_option('display.max_columns', None) # 显示所有列
+warnings.filterwarnings('ignore')
 
 def open_file():
     df=pd.read_csv('report.csv')
@@ -41,30 +43,29 @@ def totle_sales_amount(df):
     plt.show()
 
 
-def sales(df):
+def order_creation(df):
     '''
-    按订单付款时间，统计实际成交数量
+    按订单付款时间，统计每日订单创建量
     :param df:
     :return:
     '''
-    amount=df[df['买家实际支付金额']>0][['订单付款时间','买家实际支付金额']]
-    sell_amount=amount.groupby(pd.Grouper(key='订单付款时间',freq='D'))['买家实际支付金额'].count().to_frame('实际成交数')
+    amount=df.groupby(pd.Grouper(key='订单付款时间',freq='D'))['订单创建时间'].count().to_frame('订单创建数')
 
     # 设置x轴坐标
-    _x=[f'{m}月{i}日' for m,i in zip(sell_amount.index.month, sell_amount.index.day)]
+    _x=[f'{m}月{i}日' for m,i in zip(amount.index.month, amount.index.day)]
 
     # 画图部分
     plt.figure(figsize=(16,9),dpi=160)
-    plt.plot(range(len(_x)),sell_amount['实际成交数'])
+    plt.plot(range(len(_x)),amount['订单创建数'])
     # 设置数据文本标签
-    plt.text(0,sell_amount['实际成交数'].max(),f'2月1日-3月1日实际成交订单数为:{sell_amount.sum().values[0]}',fontsize=20)
-    for x,y in zip(range(len(_x)),sell_amount['实际成交数']):
+    plt.text(0,amount['订单创建数'].max(),f'2月1日-3月1日订单创建数为:{amount.sum().values[0]}',fontsize=20)
+    for x,y in zip(range(len(_x)),amount['订单创建数']):
         plt.text(x, y+15,y,ha='center')
     plt.xticks(range(len(_x)),_x,rotation=45)
     plt.ylabel('销量')
     plt.xlabel('日期')
-    plt.title('每日成交量走势')
-    plt.savefig('每日成交量走势V1.jpg')
+    plt.title('每日订单创建走势',fontsize=20)
+    plt.savefig('每日订单创建数走势.jpg')
     plt.show()
 
 def Conversion_rates(df):
@@ -105,15 +106,20 @@ def hot_sales(df):
         '销量':df['总金额'].value_counts(),
         '全额付款销量':df[df['买家实际支付金额']==df['总金额']]['总金额'].value_counts()
     }).sort_values(by='销量',ascending=False)
-    hot_['全额付款占比']=hot_['全额付款销量']/hot_['销量']
-    hot_['占比(%)']=hot_['销量'].apply(lambda x : round((x/hot_['销量'].sum())*100,2))
-    print(f'{"-"*20}热卖前10{"-"*20}')
-    print(hot_.head(10))
+    hot_['总销量占比(%)']=hot_['销量'].apply(lambda x : round((x/hot_['销量'].sum())*100,2))
+    hot_['销售额']=df.groupby(by='总金额')['买家实际支付金额'].sum()
+    hot_['销售额占比(%)']=hot_['销售额'].apply(lambda x : round((x/hot_['销售额'].sum())*100,2))
+    print(f'{"-"*20}热卖前20{"-"*20}')
+    print(hot_.head(20))
+
     plt.figure(figsize=(16,9),dpi=160)
+    # 将销售量前10之外的品类归为其他
     a=hot_.iloc[10:].sum().to_frame('其他').unstack().unstack()
     hot_=pd.concat([hot_.head(10), a])
+    # 色板
     color=sns.color_palette('hls',11)
-    plt.pie(hot_['占比(%)'], autopct='%3.1f%%', labels=hot_.index, startangle=45,colors=color)
+    # 画图
+    plt.pie(hot_['总销量占比(%)'], autopct='%3.1f%%', labels=hot_.index, startangle=45,colors=color)
     plt.title('各品类销量情况')
     plt.savefig('各品类实际成交情况。jpg')
     plt.show()
@@ -147,32 +153,23 @@ def location_distribution(df):
 
 def location_rates(df):
     # 找到所有实际成交或全额成交的所有行
-    df['成交情况'],df['全价成交']=0,0
+    df['成交情况']=0
     df.loc[df[df['买家实际支付金额']>0].index.to_list(),'成交情况']=1
     df.loc[df[df['买家实际支付金额']==df['总金额']].index.to_list(),'全价成交']=1
 
-    # 分组聚合，按成交数进行降序
-    df_=df.groupby('收货地址').agg(
+    # 分组求和
+    df_=df.groupby(by=['收货地址',pd.Grouper(key='订单付款时间',freq='W')]).agg(
         订单创建数=('订单创建时间', 'count'),
-        订单付款数=('订单付款时间', 'count'),
-        订单成交数=('成交情况', 'sum'),
-        全额成交数=('全价成交', 'sum'),
-    ).sort_values(by=['订单成交数','全额成交数'],ascending=False)
+        订单成交数=('成交情况', 'sum')
+    )
+    df_head3=df_.loc[['上海','广东省','北京','江苏省','浙江省']]
+    # 求转化率
+    df_head3['实际成交转化率']=df_head3['订单成交数']/df_head3['订单创建数']
+    print(f'{"-" * 15}销量前5的省市订单创建量量情况{"-" * 15}\n')
+    print(df_head3['订单创建数'].unstack())
+    print(f'\n{"-" * 15}销量前5的省市的转化率情况{"-" * 15}\n')
+    print(df_head3['实际成交转化率'].unstack())
 
-    # 计算成交数总量，找出累计成交量小于整体成交量80%且成交量最多的前N个省市
-    max_=df_['订单成交数'].sum()
-    df_['累计成交数']=df_['订单成交数'].cumsum()
-    df_max=df_[df_['累计成交数']<=max_*0.8].copy()
-    index_=df_max.index
-    df_max.drop(columns='累计成交数',inplace=True)
-
-    # 计算转化率
-    df_rates = df_max.apply(lambda x: round((x / df_['订单创建数']) * 100, 2), axis=0).sort_values(by=['订单成交数','全额成交数'],ascending=False)
-
-    print(f'\n{"-" * 15}销量前{len(index_)}个省市的订单量情况{"-" * 15}\n')
-    print(df_max)
-    print(f'\n{"-" * 15}销量前{len(index_)}个省市的转化率情况{"-" * 15}\n')
-    print(df_rates.loc[index_])
 
 def main_func():
     df=open_file()
@@ -180,20 +177,20 @@ def main_func():
     # 统计日维度下的总销售额情况
     # totle_sales_amount(df)
 
-    # 时间维度下的销量情况
-    # sales(df)
-
-    # 转化率情况
+     # 转化率情况
     # Conversion_rates(df)
 
+    # 订单创建情况
+    # order_creation(df)
+
     # 热卖商品
-    hot_sales(df)
+    # hot_sales(df)
 
     # 城市分布情况
     # location_distribution(df)
 
     # 细分维度下的转化率情况
-    # location_rates(df)
+    location_rates(df)
 
 if __name__ == '__main__':
     main_func()
